dd9712817abbe7bdc50e7b46c582c746f40adbf3
Crowdbotics <team@crowdbotics.com>
2021-11-21 06:29:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "spiritual_holiday_32063",
+  url: "https://spiritual_holiday_32063.botics.co",
+  api: "https://spiritual_holiday_32063.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
